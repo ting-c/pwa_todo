e033b2489ff8ed531fc34f7994df65e0dbe1f172
ting-c <ting.cheong31@gmail.com>
2020-07-15 12:46:02 +0100

    added datetime

--- src/Task.js
+++ src/Task.js
@@ -1,24 +1,68 @@
 import React, { useState } from 'react';
+import DateTimePicker from 'react-datetime-picker';
+import dateTimeIcon from './img/datetime-icon.png';
 
 const Task = ({ task, removeTask, toggleCompleted }) => {
 
   const { title, id, completed } = task;
+
   const [taskTitle, setTitle] = useState(title);
+  const [dateTime, setDateTime] = useState(new Date());
+
   return (
-    <div className='row input-group mb-3 animate__animated animate__bounceIn'>
-      <div className="input-group-prepend">
-        <div className="input-group-text">
-          { completed ? 
-          <input type="checkbox" aria-label="Checkbox" onChange={() => toggleCompleted(id)} checked /> 
-          : 
-          <input type="checkbox" aria-label="Checkbox" onChange={() => toggleCompleted(id)} />
-          }
-        </div>
-      </div>
-      <input type="text" className="form-control" aria-label="Task title" onChange={(e) => setTitle(e.target.value)} value={taskTitle} />
-      <button className='btn btn-warning' onClick={() => removeTask(id)}>x</button>
+    <React.Fragment>
+		<div className="row input-group animate__animated animate__bounceIn">
+			<div className="input-group-prepend">
+				<div className="input-group-text">
+					{completed ? (
+						<input
+							type="checkbox"
+							aria-label="Checkbox"
+							onChange={() => toggleCompleted(id)}
+							checked
+						/>
+					) : (
+						<input
+							type="checkbox"
+							aria-label="Checkbox"
+							onChange={() => toggleCompleted(id)}
+						/>
+					)}
+				</div>
+			</div>
+			<input
+				type="text"
+				className="form-control"
+				aria-label="Task title"
+				onChange={(e) => setTitle(e.target.value)}
+				value={taskTitle}
+			/>
+			<div class="input-group-append">
+        <a
+          class="btn btn-success"
+          data-toggle="collapse"
+          href={`#collapse${id}`}
+          role="button"
+          aria-expanded="false"
+          aria-controls={`collapse${id}`}
+        >
+          <img
+            src={dateTimeIcon}
+            alt="datetime-icon"
+            style={{ height: "1rem" }}
+          />
+        </a>        
+        <button className="btn btn-warning" onClick={() => removeTask(id)}>
+          x
+        </button>
+			</div>
+
+		</div>
+    <div class="row mb-3 collapse" id={`collapse${id}`}>
+      <DateTimePicker onChange={setDateTime} value={dateTime} className={["shadow-sm", ""]} />
     </div>
-  )
+    </React.Fragment>
+	);
 }
 
 export default Task;
--- src/__tests__/TaskContainer.test.js
+++ src/__tests__/TaskContainer.test.js
@@ -5,15 +5,34 @@ import TaskContainer from '../TaskContainer';
 
 describe('TaskContainer component', () => {
 
-  const utils = render(<TaskContainer />);
-  const add_task_input = utils.getByTestId('add-task-input');
-  fireEvent.change(add_task_input, { target: { value: "Title 1" } });
-  fireEvent.submit(add_task_input);
-  const tasks_element = utils.getByTestId('tasks');
+  let tasks_element;
+  let completed_tasks_element;
+  beforeEach(() => {
+    const utils = render(<TaskContainer />);
+    const add_task_input = utils.getByTestId('add-task-input');
+    fireEvent.change(add_task_input, { target: { value: "Title 1" } });
+    fireEvent.submit(add_task_input);
+    tasks_element = utils.getByTestId('tasks');
+    completed_tasks_element = utils.getByTestId('completed-tasks');   
+  })
   
   test('should render task with correct title', () => {
     const input_element = tasks_element.getElementsByTagName('input')[1];
     expect(input_element.value).toBe('Title 1');
+  });
+
+  test('should render the completed task in completed-tasks when checkbox is checked', () => {
+    const checkbox = tasks_element.getElementsByTagName("input")[0];
+    fireEvent.click(checkbox);
+    const input_element = completed_tasks_element.getElementsByTagName('input')[1];
+    expect(input_element.value).toBe('Title 1');
+  });
+
+  test('should NOT render the completed task in tasks when checkbox is checked', () => {
+    const checkbox = tasks_element.getElementsByTagName("input")[0];
+    fireEvent.click(checkbox);
+    const input_element = tasks_element.getElementsByTagName('input')[1];
+    expect(input_element).toBeUndefined();
   });
 
 })
