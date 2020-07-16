b03fde6c49a386e08a35e00a2844bf866a2c7236
ting-c <ting.cheong31@gmail.com>
2020-07-16 13:58:46 +0100

    updated service worker and manifest and added logo img

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = ['index.html'];
+const urlsToCache = ['/'];
 
 const self = this;
 
--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import TaskContainer from './TaskContainer';
 const App = () => {
   return (
     <div className='container'>
-      <h1 className='row d-flex justify-content-center text-center m-3'>Todo List</h1>
       <TaskContainer />
     </div>
   )
